899687deb67e9ccb84bcbf6d872d387a0c2755d7
Adib Faqih <foezst@gmail.com>
2020-07-05 06:46:52 +0700

    server update

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const cors = require('cors');
 
 const router = require('./routes');
 
+app.use(cors());
 app.use(router);
 
 app.listen(PORT, () => console.log(`App running on PORT ${PORT}`));
